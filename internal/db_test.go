0a0b1c8771d0b8b6889c9f16a8314dc45262be5a
asher duarte <dev81log@gmail.com>
2023-05-27 17:34:40 -0300

    add db_test

---
+++ internal/db_test.go
@@ -0,0 +1,36 @@
+package internal
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	// Mock environment variables
+	os.Setenv("POSTGRES_USER", "username")
+	os.Setenv("POSTGRES_PASSWORD", "password")
+	os.Setenv("POSTGRES_DB", "dbname")
+	os.Setenv("POSTGRES_HOST", "localhost")
+	os.Setenv("POSTGRES_PORT", "5432")
+
+	var logOutput bytes.Buffer
+	log.SetOutput(&logOutput)
+
+	Init()
+
+	// Check if log contains success message and if db is not nil
+	assert.Contains(t, logOutput.String(), "Successfully connected!")
+
+	assert.NotNil(t, db)
+
+	// Reset environment variables
+	os.Unsetenv("POSTGRES_USER")
+	os.Unsetenv("POSTGRES_PASSWORD")
+	os.Unsetenv("POSTGRES_DB")
+	os.Unsetenv("POSTGRES_HOST")
+	os.Unsetenv("POSTGRES_PORT")
+}
