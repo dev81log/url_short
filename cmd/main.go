1e8db918168bab0733bd2efd8734fe1e52e1b70d
asher duarte <dev81log@gmail.com>
2023-05-28 18:49:58 -0300

    Refactor main and rules files

--- cmd/main.go
+++ cmd/main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/dev81log/url_short/internal"
 	"github.com/gin-gonic/gin"
 )
-
-type requestPayload struct {
-	URL string `json:"url" binding:"required"`
-}
-
-func shortenURL(c *gin.Context) {
-	var payload requestPayload
-
-	if err := c.ShouldBindJSON(&payload); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	code := internal.GenerateUniqueCode()
-
-	err := internal.InsertURLAndCode(payload.URL, code)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not insert URL and code into database."})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"code": code})
-}
-
-func redirectURL(c *gin.Context) {
-	code := c.Param("code")
-
-	url, err := internal.GetURLFromCodeIfNotExpired(code)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.Redirect(http.StatusMovedPermanently, url)
-}
 
 func main() {
 	internal.Init()
 
 	r := gin.Default()
 
-	r.POST("/", shortenURL)
-	r.GET("/:code", redirectURL)
+	r.POST("/", internal.ShortenURL)
+	r.GET("/:code", internal.RedirectURL)
 
 	r.Run()
 }
--- internal/rules.go
+++ internal/rules.go
@@ -3,7 +3,10 @@ package internal
 import (
 	"errors"
 	"math/rand"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -32,4 +35,39 @@ func GetURLFromCodeIfNotExpired(code string) (string, error) {
 	}
 
 	return url, nil
+}
+
+type requestPayload struct {
+	URL string `json:"url" binding:"required"`
+}
+
+func ShortenURL(c *gin.Context) {
+	var payload requestPayload
+
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	code := GenerateUniqueCode()
+
+	err := InsertURLAndCode(payload.URL, code)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not insert URL and code into database."})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"code": code})
+}
+
+func RedirectURL(c *gin.Context) {
+	code := c.Param("code")
+
+	url, err := GetURLFromCodeIfNotExpired(code)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Redirect(http.StatusMovedPermanently, url)
 }
